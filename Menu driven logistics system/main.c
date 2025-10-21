f6fe90e6c520e2fa2577f69de5571a1bbba02f0d
Anjana-Randeer <as20240626@sci.sjp.ac.lk>
2025-10-22 00:39:08 +0530

    Built the City Management  function

--- Menu driven logistics system/main.c
+++ Menu driven logistics system/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int cityManagement(char *cityNames,int x);
+
+int cityManagement(char *cityNames,int x)
+{
+    printf("Type \"Done\" when complete entering the data");
+
+}
+
 int main()
 {
+    int choice;
+    char *cityNames[30][50];
+    int x=30;
+
     printf("---Logistics Management System---\n");
     printf("\n---------------------------------\n");
     printf("\n01. Manage Cities\n");
@@ -12,6 +24,9 @@ int main()
     printf("05. Find Least-Cost Route for a Delivery\n");
     printf("06. View Performance Reports\n");
     printf("07. Exit\n");
+
+    printf("\nEnter your choice:");
+    scanf("%d",&choice);
 
     return 0;
 
