237a8960a3fbd3477a069fe9a880fd6cf6059321
Anjana-Randeer <as20240626@sci.sjp.ac.lk>
2025-10-21 20:57:41 +0530

    Created the Menu for the program

--- Menu driven logistics system/main.c
+++ Menu driven logistics system/main.c
@@ -3,6 +3,16 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("---Logistics Management System---\n");
+    printf("\n---------------------------------\n");
+    printf("\n01. Manage Cities\n");
+    printf("02. Manage Distances\n");
+    printf("03. New delivery request\n");
+    printf("04. Cost, Time, and Fuel Calculations\n");
+    printf("05. Find Least-Cost Route for a Delivery\n");
+    printf("06. View Performance Reports\n");
+    printf("07. Exit\n");
+
     return 0;
+
 }
